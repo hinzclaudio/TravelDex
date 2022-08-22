b270dba6177bf789fdaae77aad8f90d32fd490c2
Claudio Hinz <hinzclaudio@googlemail.com>
2022-08-22 11:03:45 +0200

    FEAT: Add EditTripViewModelTests

--- TravelWeatherTests/ViewModelTests/EditTripViewModelTests.swift
+++ TravelWeatherTests/ViewModelTests/EditTripViewModelTests.swift
@@ -13,14 +13,20 @@ import XCTest
 
 class EditTripViewModelTests: XCTestCase {
     
+    var mockDependencies: MockDependencies!
+    var viewModel: EditTripViewModelType!
+    
     override func setUpWithError() throws {
         try super.setUpWithError()
-        // TODO: Create Dependencies
+        self.mockDependencies = MockDependencies()
+        self.viewModel = EditTripViewModel(dependencies: mockDependencies)
     }
     
     
-    func testExample() throws {
-        // TODO: Implement.
+    func testUpdateTripIsForwardedToStore() throws {
+        let mockTrip = Trip(id: TripID(), title: "Mocked Title", visitedLocations: [])
+        let _ = viewModel.update(.just(mockTrip))
+        XCTAssertTrue(mockDependencies.mockTripStore.addTripCalled)
     }
     
 }
